ddaa5d8235f95d523c91f559f598467ed3938e45
Madhawa Munasinghe <madhawaprageeth86@gmail.com>
2022-12-21 14:57:09 +0530

    Selection Container added

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,45 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import logo from '../images/logo-designcode.svg'
-import './header.css'
+import logo from "../images/logo-designcode.svg"
+import "./header.css"
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img src={logo} width="30" /></Link>
-      <Link to="/cources">Cources</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy"><button>Buy</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasScrolled : false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset;
+    if (scrollTop >50) {
+      this.setState({hasScrolled : true})
+    } else {
+      this.setState({hasScrolled : false})
+    }
+  }
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/">
+            <img src={logo} width="30" />
+          </Link>
+          <Link to="/cources">Cources</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy">
+            <button>Buy</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
